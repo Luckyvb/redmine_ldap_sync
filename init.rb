796d536661c7f3687aa314b5f910cf024f1c28eb
user <user@by01-unweb04.ts.local>
2020-05-16 22:53:12 +0300

    -

--- init.rb
+++ init.rb
@@ -3,21 +3,20 @@ require 'redmine'
 Redmine::Plugin.register :redmine_ldap_sync do
   name 'Redmine LDAP Sync'
   author 'Ricardo Santos'
-  author_url 'https://github.com/thorin'
+  author_url 'https://github.com/luckyvb'
   description 'Syncs users and groups with ldap'
-  url 'https://github.com/thorin/redmine_ldap_sync'
-  version '2.1.1.devel'
+  url 'https://github.com/luckyvb/redmine_ldap_sync'
+  version '2.1.2.1'
   requires_redmine :version_or_higher => '2.1.0'
 
   settings :default => HashWithIndifferentAccess.new()
-  menu :admin_menu, :ldap_sync, { :controller => 'ldap_settings', :action => 'index' }, :caption => :label_ldap_synchronization,
-                    :html => {:class => 'icon icon-ldap-sync'}
+  menu :admin_menu, :ldap_sync, { :controller => 'ldap_settings', :action => 'index' }, :caption => :label_ldap_synchronization, :html => {:class => 'icon icon-ldap-sync'}
 end
 
-RedmineApp::Application.config.after_initialize do
+#RedmineApp::Application.config.after_initialize do
   require_dependency 'ldap_sync/core_ext'
   require_dependency 'ldap_sync/infectors'
-end
+#end
 
 # hooks
 require_dependency 'ldap_sync/hooks'
